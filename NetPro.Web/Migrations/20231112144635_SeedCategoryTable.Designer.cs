ff9a604b8040c07518a25883a26f7cfd3be3538a
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-11-12 23:37:54 +0530

    fetch data from databse and shoq data

--- NetPro.Web/Migrations/20231112144635_SeedCategoryTable.Designer.cs
+++ NetPro.Web/Migrations/20231112144635_SeedCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetPro.Web.Data;
 
@@ -10,9 +11,11 @@ using NetPro.Web.Data;
 namespace NetPro.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231112144635_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
